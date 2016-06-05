9355fffe0628bfe5ee92986870d2f1673577aa9e
Chylli <chylli@binary.com>
2016-06-05 08:05:20 +0800

    parse args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod netcat;
+use self::netcat::args::*;
+use std::env;
+
+//let args: Vec<String> = env::args().collect();
+//let args = env::args().collect::<Vec<String>>();
+
 fn main() {
-    println!("Hello, world!");
+    match parse(env::args().collect()){
+        Ok(args) => match args {
+            Args::SendMode(s,p) => {
+                println!("send mode on {} {}",s, p);
+            },
+            Args::ListenMode(p) => {
+                println!("listen mode on {}", p);
+            },
+            Args::Usage(program,opt) => {
+                usage(&program, opt);
+            }
+        },
+        Err(err) => println!("{}",err)
+    };
+
 }
---
+++ src/netcat/args.rs
@@ -0,0 +1,49 @@
+extern crate getopts;
+
+use self::getopts::Options;
+
+pub enum Args {
+    SendMode(String, u16),
+    ListenMode(u16),
+    Usage(String, Options),
+}
+
+pub fn usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options]", program);
+    println!("{}",opts.usage(&brief))
+}
+
+pub fn parse(args: Vec<String>) -> Result<Args, String> {
+    let program = &args[0];
+    let mut opts = Options::new();
+    opts.optflag("h","help", "print usage info");
+    opts.optopt("l", "", "listen mode on specified TCP-port", "PORT");
+
+    let matches = try!(opts.parse(&args[1..]).map_err(|err|err.to_string()));
+    if matches.opt_present("h"){
+        return Ok(Args::Usage(program.clone(), opts));
+    }
+
+    if matches.opt_present("l") {
+        let listen_port: u16 =
+            try!(
+                try!(
+                    matches.opt_str("l").ok_or("You should specify TCP-port for listen mode.") // ok_or ?
+                ).parse().map_err(|err| "The port should be an integer.")
+            );
+        Ok(Args::ListenMode(listen_port))
+    }
+    else {
+        if matches.free.len() == 0 && !matches.opt_present("h"){
+            Ok(Args::Usage(program.clone(), opts))
+        }
+        else if matches.free.len() < 2 {
+            Err("You should specify host name and TCP-port for send mode.".to_owned())
+        }
+        else {
+            let hostname = &matches.free[0];
+            let port: u16 = try!(matches.free[1].parse().map_err(|err| "Port should be an ingeter"));
+            Ok(Args::SendMode(hostname.clone(), port))
+        }
+    }
+}
